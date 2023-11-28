6ee3f7679f6f5ccfe05454f6983c1592d65daabf
ryutaro <m2002r1028@icloud.com>
2023-11-28 19:16:44 +0900

    add: 通信時間を指数分布に拡張できるようにしておいた

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import random
+
+
 class Network():
     def __init__(self) -> None:
         self.networks = [
@@ -45,11 +49,9 @@ class Network():
         """
         ランダムなノードを2つ返す.
         """
-        # self.networksの長さを取得
-        length = len(self.networks)
-        # 0, length-1の間でランダムな数値を2つ取得
-        s_node = 0
-        e_node = 0
+        node_num = len(self.networks)
+        nodes = list(range(node_num))
+        s_node, e_node = random.sample(nodes, 2)
         return s_node, e_node
 
     def start(self, s_node: int, e_node: int) -> None:
@@ -127,25 +129,54 @@ class Network():
 
 
 class Communication():
-    def __init__(self, network: Network, algorithm: int) -> None:
+    def __init__(self, network: Network, s_node: int, e_node: int, algorithm: int) -> None:
         self.network = network
         # 1: 最小ホップ経路を用いた固定経路
         # 2: 最大路を用いた固定経路
         # 3: 最小ホップ経路を用いた要求時経路
         # 4: 最大路を用いた要求時経路
-        self.algorithm = algorithm
-        self.path = []
+        self.path = self.set_path(s_node, e_node, algorithm)
+        # 指数分布に従う通信時間
+        self.communication_time = 0
+        self.set_communication_time_by_expovariate()
 
-    def set_path(self, s_node: int, e_node: int):
+    def set_communication_time_by_expovariate(self, average: int = 10) -> None:
+        """
+        指数分布に従う通信時間を返す.
+
+        average: 平均
+        """
+        self.communication_time = round(random.expovariate(1/average))
+
+    def set_communication_time_by_int(self, communication_time: int) -> None:
+        """
+        通信時間を設定する.
+
+        communication_time: 通信時間
+        """
+        self.communication_time = communication_time
+
+    def get_communication_time(self) -> int:
+        """
+        通信時間を返す.
+        """
+        return self.communication_time
+
+    def set_path(self, s_node: int, e_node: int, algorithm: int) -> list:
         """
         経路の決定.
 
         s_node: 開始ノード
         e_node: 終了ノード
+        algorithm: 経路の決定方法
+        1 => 最小ホップ経路を用いた固定経路
+        2 => 最大路を用いた固定経路
+        3 => 最小ホップ経路を用いた要求時経路
+        4 => 最大路を用いた要求時経路
         """
         return []
 
-    def start(self, s_node: int, e_node: int) -> bool:
+    def start(self) -> bool:
         """
         通信の開始. もし容量があれば容量を1減らし、Trueを返す.
 
@@ -154,7 +185,7 @@ class Communication():
         """
         return False
 
-    def end(self, s_node: int, e_node: int) -> None:
+    def end(self) -> None:
         """
         通信の終了. 容量を増やす.
 
@@ -165,9 +196,8 @@ class Communication():
 
 
 class CommunicationManager():
-    def __init__(self, communication_time: int) -> None:
+    def __init__(self, communication_time: int = 0) -> None:
         self.network = Network()
-        self.communication_time = communication_time
         # 1: 最小ホップ経路を用いた固定経路
         # 2: 最大路を用いた固定経路
         # 3: 最小ホップ経路を用いた要求時経路
@@ -177,33 +207,83 @@ class CommunicationManager():
         self.end_num = 0
         self.MAX_START_NUM = 10000
         self.communication = []
+        self.communication_time = communication_time
+        if self.communication_time < 0:
+            raise Exception("通信時間は0以上である必要があります。")
+        self.communicaton_end_schedule = {}
+        self.out_file = f"data/{self.ALGORITHM}_out.csv"
+        self.setup()
 
-    def run(self) -> None:
+    def setup(self) -> None:
         """
-        通信を実行する.
+        出力ファイルの作成.
         """
-        for i in range(self.MAX_START_NUM):
-            # 通信の開始
-            # s_node, e_node = self.network.get_random_nodes()
-            # communication = Communication(self.network, self.ALGORITHM)
-            # if communication.start(s_node, e_node):
-            #    self.start_num += 1
-            #    self.communication.append(communication)
-            # else:
-            #    self.communication.append(None)
-            # (i-communication_time)番目の通信の終了
-            pass
+        # dataフォルダがなければ作成
+        if not os.path.isdir("data"):
+            os.mkdir("data")
+        # data/{ALGORITHM}_out.csvがなければ作成
+        if not os.path.isfile(self.out_file):
+            with open(self.out_file, "w") as f:
+                f.write("communication_time, loss\n")
 
     def save(self) -> None:
         """
         通信を保存する.
         """
-        # TODO: data/{ALGORITHM}_out.csvに追記
-        # TODO: time, 1-(start_num/MAX_START_NUM)
-        pass
+        # data/{ALGORITHM}_out.csvに追記
+        with open(self.out_file, "a") as f:
+            f.write(f"{self.communication_time}, {self.loss()}\n")
 
+    def loss(self) -> float:
+        """
+        通信のロス率を返す.
+        """
+        return (self.MAX_START_NUM - self.start_num) / self.MAX_START_NUM
+
+    def run(self) -> None:
+        """
+        通信を実行する.
+        """
+        # TODO: while文を回すように変更
+        # TODO: で、MAX_START_NUM回通信を開始する。
+        # TODO: そして通信がすべて終了したらwhile文を抜ける。
+        for i in range(self.MAX_START_NUM):
+            print(f"==={i}回目の通信===")
+
+            s_node, e_node = self.network.get_random_nodes()
+            communication = Communication(self.network, s_node, e_node, self.ALGORITHM)
+            if self.communication_time > 0:
+                communication.set_communication_time_by_int(self.communication_time)
+
+            # 通信の開始
+            if communication.start():
+                self.start_num += 1
+                self.communication.append(communication)
+                communication_time = communication.get_communication_time()
+                if i+communication_time in self.communicaton_end_schedule:
+                    self.communicaton_end_schedule[i+communication_time].append(communication)
+                else:
+                    self.communicaton_end_schedule[i+communication_time] = [communication]
+            else:
+               self.communication.append(None)
+
+            # 通信の終了
+            if i in self.communicaton_end_schedule:
+                for communication in self.communicaton_end_schedule[i]:
+                    communication.end()
+                    self.end_num += 1
+                self.communicaton_end_schedule.pop(i)
+
+        # networkとcurrent_networkが一致していなければエラー
+        if self.network.get() != self.network.get_current():
+            raise Exception("networkとcurrent_networkが一致しません。")
+
+        # 通信の保存
+        self.save()
 
 if __name__ == "__main__":
     network = Network()
     network.show()
     network.show_current()
+    cm = CommunicationManager()
+    cm.run()
