7ed560a7a3f94f09e1fc7abe0e141b1b1d56057d
ryutaro <m2002r1028@icloud.com>
2023-12-02 20:37:37 +0900

    fix: すべての経路選択方式のデータを同じグラフに描画

--- evaluation.py
+++ evaluation.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = 'data/confirm/algorithm_1_out.csv'
+file_path = [
+    'data/confirm/algorithm_1_out.csv',
+    'data/confirm/algorithm_2_out.csv',
+    'data/confirm/algorithm_3_out.csv',
+    'data/confirm/algorithm_4_out.csv',
+]
 
-# CSVファイルからデータを読み込む
-df = pd.read_csv(file_path, encoding='utf-8')
-print(df.head())
+algorithms = [
+    '1: static shortest path',
+    '2: static widest path',
+    '3: dynamic shortest path',
+    '4: dynamic widest path'
+]
 
-# lossの平均値を計算
-average_loss = df.groupby('service_time')['loss'].mean().reset_index()
-print(average_loss.head())
-
-# グラフの作成
-plt.plot(average_loss['service_time'], average_loss['loss'], marker='.')
 plt.xlabel('Service Time')
 plt.ylabel('Average Loss')
 plt.grid(True)
+
+for i, path in enumerate(file_path):
+    df = pd.read_csv(path, encoding='utf-8')
+    # lossの平均値を計算
+    average_loss = df.groupby('service_time')['loss'].mean().reset_index()
+    # グラフの描画
+    plt.plot(average_loss['service_time'], average_loss['loss'], marker='.', label=algorithms[i])
+
+plt.legend()
 plt.show()
