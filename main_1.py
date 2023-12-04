bf63aeb95d88158bb4d8a18663bd912d200a2d75
ryutaro <m2002r1028@icloud.com>
2023-12-05 01:49:25 +0900

    fix: 実験1と実験2で分けた

--- main_1.py
+++ main_1.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     # ==========
     # 実験1（サービス時間、到着間隔を固定する場合）
     # ==========
-    # 実験回数.。初期値は10
+    # 実験回数。初期値は10
     experiment_num = 10
+
     # アルゴリズム。初期値は[1, 2, 3, 4, 5, 6]
     # 1: static shortest path
     # 2: static widest path
@@ -15,6 +16,7 @@ if __name__ == "__main__":
     # 5: static shortest widest path
     # 6: dynamic shortest widest path
     algorithms = [1, 2, 3, 4, 5, 6]
+
     # サービス時間。初期値は1から500までの整数
     service_times = [i for i in range(1, 501)]
 
@@ -27,27 +29,3 @@ if __name__ == "__main__":
                 cm = CM(algorithm=algorithm, service_time=service_time)
                 cm.run()
             print(f"time: {datetime.datetime.now() - now}")
-
-    # ==========
-    # 実験2(指数分布に従うサービス時間、到着間隔を生成する場合)
-    # ==========
-    # 実験回数。初期値は10
-    # experiment_num = 10
-    # # アルゴリズム。初期値は[1, 2, 3, 4, 5]
-    # # 1: static shortest path
-    # # 2: static widest path
-    # # 3: dynamic shortest path
-    # # 4: dynamic widest path
-    # # 5: static shortest widest path
-    # # 6: dynamic shortest widest path
-    # algorithms = [2]
-    # average_service_times = [0]
-    # average_arrival_interval = [0]
-
-    # for algorithm in algorithms:
-    #     print(f"algorithm: {algorithm}")
-    #     for average_service_time in average_service_times:
-    #         for average_arrival_interval in average_arrival_interval:
-    #             for _ in range(experiment_num):
-    #                 cm = CM(algorithm=algorithm, average_service_time=average_service_time, average_arrival_interval=average_arrival_interval)
-    #                 cm.run()
---
+++ main_2.py
@@ -0,0 +1,35 @@
+from communication_manager import CommunicationManager as CM
+import datetime
+
+if __name__ == "__main__":
+    # ==========
+    # 実験2(指数分布に従うサービス時間、到着間隔を生成する場合)
+    # ==========
+    # 実験回数。初期値は10
+    experiment_num = 10
+
+    # アルゴリズム。初期値は[1, 2, 3, 4, 5]
+    # 1: static shortest path
+    # 2: static widest path
+    # 3: dynamic shortest path
+    # 4: dynamic widest path
+    # 5: static shortest widest path
+    # 6: dynamic shortest widest path
+    algorithms = [1, 2, 3, 4, 5, 6]
+
+    # 平均サービス時間。初期値は[i for i in range(1, 11)]
+    average_service_times = [i for i in range(1, 11)]
+
+    # 平均到着間隔。初期値は[i for i in range(1, 11)]
+    average_arrival_interval = [i for i in range(1, 11)]
+
+    for algorithm in algorithms:
+        print(f"algorithm: {algorithm}")
+        for average_service_time in average_service_times:
+            for average_arrival_interval in average_arrival_interval:
+                print(f"average_service_time: {average_service_time}, average_arrival_interval: {average_arrival_interval}", end=", ")
+                now = datetime.datetime.now()
+                for _ in range(experiment_num):
+                    cm = CM(algorithm=algorithm, average_service_time=average_service_time, average_arrival_interval=average_arrival_interval)
+                    cm.run()
+                print(f"time: {datetime.datetime.now() - now}")
