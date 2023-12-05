48091826b104efd5b748b5a366a6ada95d036517
ryutaro <m2002r1028@icloud.com>
2023-12-06 04:26:32 +0900

    fix: 変数名のミス

--- main_2.py
+++ main_2.py
@@ -17,17 +17,20 @@ if __name__ == "__main__":
     # 5: static shortest widest path
     # 6: dynamic shortest widest path
     algorithms = [1, 2, 3, 4, 5, 6]
+    # algorithms = [1, 2]
+    # algorithms = [3, 4]
+    # algorithms = [5, 6]
 
     # 平均サービス時間。初期値は[i for i in range(1, 11)]
-    average_service_times = [i for i in range(1, 11)]
+    average_service_times = [i for i in range(1, 101)]
 
     # 平均到着間隔。初期値は[i for i in range(1, 11)]
-    average_arrival_interval = [i for i in range(1, 11)]
+    average_arrival_intervals = [10,]
 
     for algorithm in algorithms:
         print(f"algorithm: {algorithm}")
         for average_service_time in average_service_times:
-            for average_arrival_interval in average_arrival_interval:
+            for average_arrival_interval in average_arrival_intervals:
                 print(f"average_service_time: {average_service_time}, average_arrival_interval: {average_arrival_interval}", end=", ")
                 now = datetime.datetime.now()
                 for _ in range(experiment_num):
