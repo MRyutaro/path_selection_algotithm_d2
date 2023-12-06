1a8bdc090cb461817204b9358d6c88faff083613
ryutaro <m2002r1028@icloud.com>
2023-12-07 03:22:56 +0900

    fix

--- evaluation_2.py
+++ evaluation_2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-experiment = 3
+experiment = 4
 
 file_path = [
     f'data/ex2/{experiment}/algorithm_1_out.csv',
@@ -10,7 +10,7 @@ file_path = [
     f'data/ex2/{experiment}/algorithm_3_out.csv',
     f'data/ex2/{experiment}/algorithm_4_out.csv',
     f'data/ex2/{experiment}/algorithm_5_out.csv',
-    f'data/ex2/{experiment}/algorithm_6_out.csv'
+    # f'data/ex2/{experiment}/algorithm_6_out.csv'
 ]
 
 algorithms = [
@@ -19,7 +19,7 @@ algorithms = [
     '3: dynamic shortest path',
     '4: dynamic widest path',
     '5: static shortest widest path',
-    '6: dynamic shortest widest path'
+    # '6: dynamic shortest widest path'
 ]
 
 plt.xlabel('Average Service Time / Average Arrival Interval')
@@ -28,13 +28,12 @@ plt.grid(True)
 
 for i, path in enumerate(file_path):
     df = pd.read_csv(path, encoding='utf-8')
-    # サーバ稼働率の平均値を計算
-    # サーバ稼働率 = 平均サービス時間 / 平均到着間隔
-    df['server_usage'] = df['average_service_time'] / df['average_arrival_interval']
+    # 平均客数 = 平均サービス時間 / 平均到着間隔
+    df['average_customers'] = df['average_service_time'] / df['average_arrival_interval']
     # lossの平均値を計算
-    average_loss = df.groupby('server_usage')['loss'].mean().reset_index()
+    average_loss = df[['average_customers', 'loss']].groupby('average_customers').mean().reset_index()
     # グラフの描画
-    plt.plot(average_loss['server_usage'], average_loss['loss'], label=algorithms[i])
+    plt.plot(average_loss['average_customers'], average_loss['loss'], label=algorithms[i])
 
 plt.legend()
 plt.show()
